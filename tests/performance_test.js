d5581035b7710076f19388a838f22c1cd9699b2a
Chris <cullm001@ucr.edu>
2024-08-01 15:02:11 -0700

    updated for github actions

--- tests/performance_test.js
+++ tests/performance_test.js
@@ -1,14 +1,14 @@
 import http from 'k6/http';
+import {check} from 'k6';
+
 
 export const options = {
     vus: 100,
     duration: '1s',
 };
 
-export default function () {
-    const url = 'API_ENDPOINT';
-
-    const node_info = {
+function gen_node_info() {
+    return {
         time: new Date().toISOString().slice(0, 19).replace('T', ' '),
         node_id: `node_${Math.floor(Math.random() * 9000) + 1000}`,
         cpu_usage: (Math.random() * 100).toFixed(2),
@@ -16,8 +16,12 @@ export default function () {
         bandwidth_usage: (Math.random() * 100).toFixed(2),
         diskspace_usage: (Math.random() * 100).toFixed(2)
     };
+}
 
-    const payload = JSON.stringify({ node_info });
+export default function () {
+    const url = __ENV.API_ENDPOINT
+  
+    const payload = JSON.stringify({ node_info: gen_node_info() });
 
     const params = {
         headers: {
@@ -26,5 +30,9 @@ export default function () {
     };
 
     const res = http.post(url, payload, params);
+    check (res, {
+        'is status 200' : (r)=>r.status ===200,
+        'payload inserted successfully' : (r) => r.body.includes("node_info inserted successfully")
+    });
 
 }
